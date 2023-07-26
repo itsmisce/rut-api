e0ef4eadd07e23ba6b950772271926c63216f540
DiegoTorres <miscelanneo@gmail.com>
2023-07-26 19:04:05 -0400

    [FEATURES] Methods

--- controllers/rutControllers.js
+++ controllers/rutControllers.js
@@ -5,24 +5,27 @@ exports.getRut = (req, res) => {
     try {
         const cantidadRuts = parseInt(req.query.cantidad);
         if (!cantidadRuts || cantidadRuts < 1) {
-            return res.status(400).json({ error: 'Debes ingresar una cantidad válida de RUTs a generar (mayor a 0).' });
+          return res.status(400).json({ error: 'Debes ingresar una cantidad válida de RUTs a generar (mayor a 0).' });
         }
-
-        const rutsGenerados = [];
+      
+        const rutGenerator = new RutGenerator();
+        const rutsGenerados = new Set();
+      
         for (let i = 0; i < cantidadRuts; i++) {
-            const rutGenerado = rutGenerator.generarRutUnico();
-            rutsGenerados.push(rutGenerado);
+          const rutGenerado = rutGenerator.generarRutUnico();
+          rutsGenerados.add(rutGenerado);
         }
+      
         const status = 200;
         const link = 'https://rut-api.vercel.app/api';
         res.setHeader('Content-Type', 'application/json');
         res.setHeader('Charset', 'utf-8');
-        return res.json({status, link, ruts: rutsGenerados });
-    } catch (error) {
+        return res.json({ status, link, ruts: Array.from(rutsGenerados) });
+      } catch (error) {
         const status = error.status || 500;
-        const link = 'https://rut-api.vercel.app/api';
         res.setHeader('Content-Type', 'application/json');
         res.setHeader('Charset', 'utf-8');
-        return res.status(status).json({ error: 'Ocurrió un error al generar los RUTS.', status, link });
-    }
+        return res.status(status).json({ error: error.message, status });
+      }
+      
 };
--- utils/ruts.js
+++ utils/ruts.js
@@ -21,13 +21,13 @@ class RutGenerator {
       const rutReverso = [...rutSinDigito].reverse();
       const serie = [2, 3, 4, 5, 6, 7];
       let suma = 0;
-
+  
       for (let i = 0; i < rutReverso.length; i++) {
         const digito = parseInt(rutReverso[i], 10);
         const multiplicador = serie[i % serie.length];
         suma += digito * multiplicador;
       }
-
+  
       const resto = suma % 11;
       const digitoVerificador = 11 - resto;
       return digitoVerificador === 11 ? 0 : digitoVerificador === 10 ? 'K' : digitoVerificador;
@@ -35,6 +35,7 @@ class RutGenerator {
       throw new Error('Ocurrió un error al calcular el dígito verificador.');
     }
   }
+  
 
   generarRutConDigito() {
     try {
